7ca3de1b6431cb984e983529b76439379227765b
John Troy Hurteau <troy.hurteau@gmail.com>
2019-01-16 13:15:27 -0500

    Update local-dev.js

--- dev/local-dev.js
+++ dev/local-dev.js
@@ -11,5 +11,10 @@ $(function(){
   $('#candy').on('click', function(){$('#candy-target').toggleClass('active'); return false;});
   $('body').append('<div id="candy-target" class="toolbag-panel panel-abs panel-solid"><ul></ul></div>')
   var candy = $('#candy-target ul');
-  candy.append('<li><a href="#">asdfasdf</a></li>');
+  populate(candy);
 });
+
+function populate(target)
+{
+ //#TODO get local-dev.json and do what it says for target.populate   
+}
